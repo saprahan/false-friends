d7e0e49708e5d4629460748d49d4ce0bedffd599
Dmitry Baskakov <saprahan@mail.ru>
2012-04-18 09:10:25 +0400

    FalseFriends service object

--- app/squeryl/DatabaseSchema.scala
+++ app/squeryl/DatabaseSchema.scala
@@ -1,27 +1,16 @@
 package squeryl
 
-
 import org.squeryl.{Schema, KeyedEntity}
 import java.sql.Timestamp
 
-
 trait DatabaseSchema extends Schema {
   val languages = table[Language]
-  val friends = table[Friend]
+  val falseFriends = table[FalseFriend]
   val friendWords = table[FriendWord]
 }
 
 object DatabaseSchema extends DatabaseSchema
 
-
-case class Friend(
-                   text: String,
-                   createdate: Timestamp
-                     ) extends KeyedEntity[Long] {
-  val id:Long = 0
-
-  def typedId = FriendId(id)
-}
 
 case class FriendWord(
                        friendId: Long,
@@ -35,8 +24,6 @@ case class FriendWord(
   def typedLanguageId = LanguageId(languageId)
 }
 
-case class LanguageId(id: Long) extends LongTypedId
-case class FriendId(id: Long) extends LongTypedId
 case class FriendWordId(id: Long) extends LongTypedId
 
 trait TypedId[T] {
